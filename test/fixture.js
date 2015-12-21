66df4e24959552c180650f136b518af48d161110
Kees Kluskens <kees@webduck.nl>
2015-12-22 00:20:00 +0100

    Oopsie after babel 6 migration

--- test/fixture.js
+++ test/fixture.js
@@ -1,3 +1,3 @@
 // backbone-relation doesn't patch Backbone on it's own, so here we do it manually,
 // so the unit tests use backbone-relation.
-Backbone.Model = window['backbone-relation'];
+Backbone.Model = window['backbone-relation'].default;
